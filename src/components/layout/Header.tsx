88a0b79ac95e5528b271b571670789c3a795a4cb
Bob <heycryptobob@gmail.com>
2022-11-01 18:00:06 +0800

    mobile header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
             <div className="flex justify-between items-center py-4">
                 <div className="flex items-center gap-8">
                     <img src={logo} className="h-16" alt="MadChad" />
-                    <h2 className="text-xl font-bold uppercase text-slate-100">Rewards</h2>
+                    <h2 className="text-xl font-bold uppercase text-slate-100 hidden sm:block">Rewards</h2>
                 </div>
                 <div className="flex gap-4 items-center">
                     <div>
